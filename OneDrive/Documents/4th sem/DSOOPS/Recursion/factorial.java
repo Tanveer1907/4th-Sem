7d02da9dd1ae48e4f7b99954f19fca0d64a7e938
Tanveer1907 <1906tanveer@gmail.com>
2026-01-21 11:51:52 +0530

    recursion practice

---
+++ OneDrive/Documents/4th sem/DSOOPS/Recursion/factorial.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class factorial {
+    public static int fact(int n){
+        if(n <=1 ) return n;
+        return n* fact(n-1);
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        int res = fact(n);
+        System.out.print(res);
+    }
+}
